f21ae8554c3f9dead4f4308cc1cf20450bf3aa60
Chinmay Pendse <pendsec@gmail.com>
2021-03-22 15:04:00 -0400

    Change models to one-to-many relationship

--- external/models.py
+++ external/models.py
@@ -8,4 +8,10 @@ class Item(models.Model):
     """Store image and text pairing."""
 
     image = models.ImageField(upload_to=image_upload_location())
-    text = models.CharField(max_length=1000)
+
+
+class Text(models.Model):
+	"""Store text captions for an image."""
+
+	image_id = models.ForeignKey('Item', on_delete=models.CASCADE)
+	text = models.CharField(max_length=1000)
